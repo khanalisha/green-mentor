b0d7a545dfdb7696f5809cd2a0680561ef321c81
khanalisha <123863034+khanalisha@users.noreply.github.com>
2024-03-23 12:57:34 +0530

    Work Update

---
+++ client/src/router/AllRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../page/Login";
+import { SignUp } from "../page/SignUp";
+
+export const AllRoute = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<SignUp />} />
+
+      {/* <Route path="/dashboard" element={<Home />} /> */}
+    </Routes>
+  );
+};
